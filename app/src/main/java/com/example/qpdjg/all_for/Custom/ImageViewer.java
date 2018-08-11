fb63d8ca5dfe0523e81d46e51aaba89a4e65afdb
WILL\triti <willtriti03@naver.com>
2018-08-12 02:37:29 +0900

    xmling content

---
+++ app/src/main/java/com/example/qpdjg/all_for/Custom/ImageViewer.java
@@ -0,0 +1,24 @@
+package com.example.qpdjg.all_for.Custom;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class ImageViewer extends LinearLayout {
+    public ImageViewer(Context context) {
+        super(context);
+    }
+
+    public ImageViewer(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ImageViewer(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public ImageViewer(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}

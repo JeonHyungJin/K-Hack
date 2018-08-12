8676d05c175f1a91d0bf7db0f81ed061de49fa42
JeonHyungJin <qpdjgjsl@gmail.com>
2018-08-12 16:14:34 +0900

    DB Doing!!!!

--- app/src/main/java/com/example/qpdjg/all_for/Fragment/CategoryDetailFragment.java
+++ app/src/main/java/com/example/qpdjg/all_for/Fragment/CategoryDetailFragment.java
@@ -34,7 +34,7 @@ public class CategoryDetailFragment extends Fragment {
     Locale lang;
     ImageView detail_icon;
     ArrayList<CategoryDetailItem> data = new ArrayList<CategoryDetailItem>();
-    ArrayList<CategoryDetailItem> delivery_data = new ArrayList<CategoryDetailItem>();
+    ArrayList<CategoryDetailItem> return_data = new ArrayList<CategoryDetailItem>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -51,17 +51,24 @@ public class CategoryDetailFragment extends Fragment {
         detail_explain = (TextView) linearLayout.findViewById(R.id.detail_explain);
         detail_list.setAdapter(categoryDetailAdapter);
 
+        return linearLayout;
+    }
+
+    public ArrayList DB_read(String datail_category) {
         /*DB*/
+        return_data.clear();
         DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
         DatabaseReference category_Ref = rootRef.child("app_category");
-        DatabaseReference delivery_Ref = category_Ref.child("delivery");
+        DatabaseReference delivery_Ref = category_Ref.child(datail_category);
         DatabaseReference delivery_apps_Ref = delivery_Ref.child("apps");
 
         ValueEventListener valueEventListener = new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for(DataSnapshot ds:dataSnapshot.getChildren()){
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    System.out.println(ds.getKey().toString().trim());
                     System.out.println(ds.child("app_img").getValue().toString().trim());
+                    return_data.add(new CategoryDetailItem(ds.getKey().toString().trim(), "sub", (float) 2.0, ds.child("app_img").getValue().toString().trim()));
                 }
             }
 
@@ -72,14 +79,7 @@ public class CategoryDetailFragment extends Fragment {
         };
         delivery_apps_Ref.addListenerForSingleValueEvent(valueEventListener);
 
-
-
-
-
-
-
-
-        return linearLayout;
+        return return_data;
     }
 
     public void setToCall(String toCall) {
@@ -110,18 +110,21 @@ public class CategoryDetailFragment extends Fragment {
     private void setTransport() {
         detail.setText("Transport");
         detail_icon.setImageResource(R.drawable.transport_black);
+        data = DB_read("transport");
         detail_explain.setText(getText(R.string.introduceTrans));
     }
 
     private void setRestaurant() {
         detail.setText("Restaurant");
         detail_icon.setImageResource(R.drawable.restaurant_black);
+        data = DB_read("restaurant");
         detail_explain.setText(getText(R.string.introduceRest));
     }
 
     private void setDelivery() {
         detail.setText("Food Delivery");
         detail_icon.setImageResource(R.drawable.food_delivery_black);
+        data = DB_read("delivery");
         detail_explain.setText(getText(R.string.introduceDeli));
 
     }
@@ -129,12 +132,14 @@ public class CategoryDetailFragment extends Fragment {
     private void setProperty() {
         detail.setText("Real Property");
         detail_icon.setImageResource(R.drawable.house_black);
+        data = DB_read("realstate");
         detail_explain.setText(getText(R.string.introducePro));
     }
 
     private void setTravel() {
         detail.setText("Travel");
         detail_icon.setImageResource(R.drawable.travel_black);
+        data = DB_read("tour");
         detail_explain.setText(getText(R.string.introduceTravel));
     }
 

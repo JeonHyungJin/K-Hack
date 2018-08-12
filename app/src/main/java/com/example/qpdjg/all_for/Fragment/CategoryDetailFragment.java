6186be34bcfbc6e352de01657b68201fbb90cd44
JeonHyungJin <qpdjgjsl@gmail.com>
2018-08-12 15:40:42 +0900

    DB doing...

--- app/src/main/java/com/example/qpdjg/all_for/Fragment/CategoryDetailFragment.java
+++ app/src/main/java/com/example/qpdjg/all_for/Fragment/CategoryDetailFragment.java
@@ -1,6 +1,7 @@
 package com.example.qpdjg.all_for.Fragment;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +15,11 @@ import com.example.qpdjg.all_for.Adater.CategoryDetailAdapter;
 import com.example.qpdjg.all_for.Custom.CustomViewPager;
 import com.example.qpdjg.all_for.Item.CategoryDetailItem;
 import com.example.qpdjg.all_for.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.*;
@@ -28,6 +34,7 @@ public class CategoryDetailFragment extends Fragment {
     Locale lang;
     ImageView detail_icon;
     ArrayList<CategoryDetailItem> data = new ArrayList<CategoryDetailItem>();
+    ArrayList<CategoryDetailItem> delivery_data = new ArrayList<CategoryDetailItem>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,6 +50,35 @@ public class CategoryDetailFragment extends Fragment {
         categoryDetailAdapter = new CategoryDetailAdapter(getContext(), R.layout.item_detail, data, viewPager);
         detail_explain = (TextView) linearLayout.findViewById(R.id.detail_explain);
         detail_list.setAdapter(categoryDetailAdapter);
+
+        /*DB*/
+        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference category_Ref = rootRef.child("app_category");
+        DatabaseReference delivery_Ref = category_Ref.child("delivery");
+        DatabaseReference delivery_apps_Ref = delivery_Ref.child("apps");
+
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot ds:dataSnapshot.getChildren()){
+                    System.out.println(ds.child("app_img").getValue().toString().trim());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+        delivery_apps_Ref.addListenerForSingleValueEvent(valueEventListener);
+
+
+
+
+
+
+
+
         return linearLayout;
     }
 
@@ -87,6 +123,7 @@ public class CategoryDetailFragment extends Fragment {
         detail.setText("Food Delivery");
         detail_icon.setImageResource(R.drawable.food_delivery_black);
         detail_explain.setText(getText(R.string.introduceDeli));
+
     }
 
     private void setProperty() {

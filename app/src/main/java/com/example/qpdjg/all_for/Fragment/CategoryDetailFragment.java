de1aaac3b32ebabbb499a984c84ef0f7adf3a388
JeonHyungJin <qpdjgjsl@gmail.com>
2018-08-12 23:00:51 +0900

    ?!?!!?!?!?!

--- app/src/main/java/com/example/qpdjg/all_for/Fragment/CategoryDetailFragment.java
+++ app/src/main/java/com/example/qpdjg/all_for/Fragment/CategoryDetailFragment.java
@@ -38,11 +38,11 @@ public class CategoryDetailFragment extends Fragment {
     String sub_String = null;
     ArrayList<CategoryDetailItem> data = new ArrayList<CategoryDetailItem>();
 
-    ArrayList<CategoryDetailItem>data1= new ArrayList<CategoryDetailItem>();
-    ArrayList<CategoryDetailItem>data2= new ArrayList<CategoryDetailItem>();
-    ArrayList<CategoryDetailItem>data3= new ArrayList<CategoryDetailItem>();
-    ArrayList<CategoryDetailItem>data4= new ArrayList<CategoryDetailItem>();
-    ArrayList<CategoryDetailItem>data5= new ArrayList<CategoryDetailItem>();
+    ArrayList<CategoryDetailItem> data1 = new ArrayList<CategoryDetailItem>();
+    ArrayList<CategoryDetailItem> data2 = new ArrayList<CategoryDetailItem>();
+    ArrayList<CategoryDetailItem> data3 = new ArrayList<CategoryDetailItem>();
+    ArrayList<CategoryDetailItem> data4 = new ArrayList<CategoryDetailItem>();
+    ArrayList<CategoryDetailItem> data5 = new ArrayList<CategoryDetailItem>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -64,28 +64,31 @@ public class CategoryDetailFragment extends Fragment {
         return linearLayout;
     }
 
-    public ArrayList DB_read(String datail_category,Context context) {
+    public ArrayList DB_read(String detail_category, Context context) {
         final ArrayList<CategoryDetailItem> return_data = new ArrayList<CategoryDetailItem>();
 
         /*DB*/
-        //System.out.println(datail_category);
         DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
         DatabaseReference category_Ref = rootRef.child("app_category");
-        DatabaseReference delivery_Ref = category_Ref.child(datail_category);
+        DatabaseReference delivery_Ref = category_Ref.child(detail_category);
         DatabaseReference delivery_apps_Ref = delivery_Ref.child("apps");
 
-        if(datail_category.equals("transport")) {
+        if (detail_category.equals("transport")) {
             sub_String = (String) context.getText(R.string.sub_transport);
-        } else if(datail_category.equals("tour")) {
+            System.out.println("여기" + sub_String);
+        } else if (detail_category.equals("tour")) {
             sub_String = (String) context.getText(R.string.sub_tour);
-        }else if(datail_category.equals("realstate")) {
+            System.out.println("여기" + sub_String);
+        } else if (detail_category.equals("realstate")) {
             sub_String = (String) context.getText(R.string.sub_realstate);
-        }else if(datail_category.equals("restaurant")) {
+            System.out.println("여기" + sub_String);
+        } else if (detail_category.equals("restaurant")) {
             sub_String = (String) context.getText(R.string.sub_restaurant);
-        }else if(datail_category.equals("delivery")) {
+            System.out.println("여기" + sub_String);
+        } else if (detail_category.equals("delivery")) {
             sub_String = (String) context.getText(R.string.sub_delivery);
+            System.out.println("여기" + sub_String);
         }
-
 
         //final String finalSub_String = sub_String;
         ValueEventListener valueEventListener = new ValueEventListener() {
@@ -97,15 +100,13 @@ public class CategoryDetailFragment extends Fragment {
                 for (DataSnapshot ds : dataSnapshot.getChildren()) {
                     int aver_rank = 0;
                     int comments_size = 0;
-                    //System.out.println(ds.getKey().toString().trim());
-                    //System.out.println(ds.child("app_img").getValue().toString().trim());
-                    for(DataSnapshot ds2 : ds.child("comments").getChildren()){
+                    for (DataSnapshot ds2 : ds.child("comments").getChildren()) {
                         aver_rank += Integer.parseInt(ds2.child("rank").getValue().toString().trim());
                         comments_size++;
                     }
-                    if(comments_size != 0 ) {
+                    if (comments_size != 0) {
                         aver_rank = aver_rank / comments_size;
-                    }else{
+                    } else {
                         aver_rank = 0;
                     }
                     return_data.add(new CategoryDetailItem(ds.getKey().toString().trim(), sub_String, aver_rank, ds.child("app_img").getValue().toString().trim()));
@@ -124,7 +125,7 @@ public class CategoryDetailFragment extends Fragment {
         return return_data;
     }
 
-    public void setToCall (String toCall){
+    public void setToCall(String toCall) {
 
         this.toCall = toCall;
 
@@ -144,25 +145,25 @@ public class CategoryDetailFragment extends Fragment {
         categoryDetailAdapter.notifyDataSetChanged();
     }
 
-    public void setViewPager (CustomViewPager viewPager){
+    public void setViewPager(CustomViewPager viewPager) {
         this.viewPager = viewPager;
     }
 
-    private void setTransport () {
+    private void setTransport() {
         detail.setText("Transport");
         detail_icon.setImageResource(R.drawable.transport_black);
         categoryDetailAdapter.setData(data1);
         detail_explain.setText(getText(R.string.introduceTrans));
     }
 
-    private void setRestaurant () {
+    private void setRestaurant() {
         detail.setText("Restaurant");
         detail_icon.setImageResource(R.drawable.restaurant_black);
         categoryDetailAdapter.setData(data2);
         detail_explain.setText(getText(R.string.introduceRest));
     }
 
-    private void setDelivery () {
+    private void setDelivery() {
         detail.setText("Food Delivery");
         detail_icon.setImageResource(R.drawable.food_delivery_black);
         categoryDetailAdapter.setData(data3);
@@ -170,26 +171,26 @@ public class CategoryDetailFragment extends Fragment {
 
     }
 
-    private void setProperty () {
+    private void setProperty() {
         detail.setText("Real Property");
         detail_icon.setImageResource(R.drawable.house_black);
         categoryDetailAdapter.setData(data5);
         detail_explain.setText(getText(R.string.introducePro));
     }
 
-    private void setTravel () {
+    private void setTravel() {
         detail.setText("Travel");
         detail_icon.setImageResource(R.drawable.travel_black);
         categoryDetailAdapter.setData(data4);
         detail_explain.setText(getText(R.string.introduceTravel));
     }
 
-    public void dataRefresh(Context context){
-        data1 = DB_read("transport",context);
-        data2 = DB_read("restaurant",context);
-        data3 = DB_read("delivery",context);
-        data4 = DB_read("tour",context);
-        data5 = DB_read("realstate",context);
+    public void dataRefresh(Context context) {
+        data1 = DB_read("transport", context);
+        data2 = DB_read("restaurant", context);
+        data3 = DB_read("delivery", context);
+        data4 = DB_read("tour", context);
+        data5 = DB_read("realstate", context);
     }
 
 }

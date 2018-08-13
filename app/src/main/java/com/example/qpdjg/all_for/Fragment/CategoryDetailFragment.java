a08cb99e967c5c1652f5ec2e8581bafa02da9ae6
JeonHyungJin <qpdjgjsl@gmail.com>
2018-08-13 15:43:11 +0900

    Sorting method

--- app/src/main/java/com/example/qpdjg/all_for/Fragment/CategoryDetailFragment.java
+++ app/src/main/java/com/example/qpdjg/all_for/Fragment/CategoryDetailFragment.java
@@ -31,6 +31,10 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Locale;
 
 public class CategoryDetailFragment extends Fragment {
@@ -47,11 +51,11 @@ public class CategoryDetailFragment extends Fragment {
     String[] item;
     ArrayList<CategoryDetailItem> data = new ArrayList<CategoryDetailItem>();
 
-    ArrayList<CategoryDetailItem>data1= new ArrayList<CategoryDetailItem>();
-    ArrayList<CategoryDetailItem>data2= new ArrayList<CategoryDetailItem>();
-    ArrayList<CategoryDetailItem>data3= new ArrayList<CategoryDetailItem>();
-    ArrayList<CategoryDetailItem>data4= new ArrayList<CategoryDetailItem>();
-    ArrayList<CategoryDetailItem>data5= new ArrayList<CategoryDetailItem>();
+    ArrayList<CategoryDetailItem> data1 = new ArrayList<CategoryDetailItem>();
+    ArrayList<CategoryDetailItem> data2 = new ArrayList<CategoryDetailItem>();
+    ArrayList<CategoryDetailItem> data3 = new ArrayList<CategoryDetailItem>();
+    ArrayList<CategoryDetailItem> data4 = new ArrayList<CategoryDetailItem>();
+    ArrayList<CategoryDetailItem> data5 = new ArrayList<CategoryDetailItem>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -70,7 +74,6 @@ public class CategoryDetailFragment extends Fragment {
         detail_list.setAdapter(categoryDetailAdapter);
 
 
-
         //spinner 언어 선택
         spinner = (Spinner) linearLayout.findViewById(R.id.spinner);
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -79,21 +82,85 @@ public class CategoryDetailFragment extends Fragment {
                 //  String show = getResources().getString(R.string.change_lang);
                 String text = spinner.getSelectedItem().toString();
                 if (text != "정렬방식") {
-                    if (text == "한국어") {
-                        lang = Locale.KOREA;
-                    } else if (text == "English") {
-                        lang = Locale.US;
-                    } else if (text == "中国語") {
-                        lang = Locale.CHINA;
-                    } else if (text == "日本語") {
-                        lang = Locale.JAPAN;
+                    if (text == "평점순서") {
+                        ArrayList temp = new ArrayList();
+                        temp.add(data1);
+                        temp.add(data2);
+                        temp.add(data3);
+                        temp.add(data4);
+                        temp.add(data5);
+
+                        for (int i = 0; i < temp.size(); i++) {
+                            Collections.sort((List<CategoryDetailItem>) temp.get(i), new Comparator<CategoryDetailItem>() {
+                                @Override
+                                public int compare(CategoryDetailItem categoryDetailItem, CategoryDetailItem t1) {
+                                    if (categoryDetailItem.getRank() > t1.getRank()) {
+                                        return 1;
+                                    } else if (categoryDetailItem.getRank() < t1.getRank()) {
+                                        return -1;
+                                    } else {
+                                        return 0;
+                                    }
+                                }
+                            });
+                            Collections.reverse((List<?>) temp.get(i));
+                        }
+                        //평점 순으로 data 1~5 정렬
+                    } else if (text == "한국인들이 많이 쓰는 순서") {
+                        ArrayList temp = new ArrayList();
+                        temp.add(data1);
+                        temp.add(data2);
+                        temp.add(data3);
+                        temp.add(data4);
+                        temp.add(data5);
+
+                        for (int i = 0; i < temp.size(); i++) {
+                            Collections.sort((List<CategoryDetailItem>) temp.get(i), new Comparator<CategoryDetailItem>() {
+                                @Override
+                                public int compare(CategoryDetailItem categoryDetailItem, CategoryDetailItem t1) {
+                                    if (categoryDetailItem.getKorean_use_rank() > t1.getKorean_use_rank()) {
+                                        return 1;
+                                    } else if (categoryDetailItem.getKorean_use_rank() < t1.getKorean_use_rank()) {
+                                        return -1;
+                                    } else {
+                                        return 0;
+                                    }
+                                }
+                            });
+                        }
+                        //한국인이 많이 쓰는 순으로 data 1~5 정렬
+                    } else if (text == "다운로드 많이 받은 순") {
+                        ArrayList temp = new ArrayList();
+                        temp.add(data1);
+                        temp.add(data2);
+                        temp.add(data3);
+                        temp.add(data4);
+                        temp.add(data5);
+
+                        for (int i = 0; i < temp.size(); i++) {
+                            Collections.sort((List<CategoryDetailItem>) temp.get(i), new Comparator<CategoryDetailItem>() {
+                                @Override
+                                public int compare(CategoryDetailItem categoryDetailItem, CategoryDetailItem t1) {
+                                    if (categoryDetailItem.getDownload_rank() > t1.getDownload_rank()) {
+                                        return 1;
+                                    } else if (categoryDetailItem.getDownload_rank() < t1.getDownload_rank()) {
+                                        return -1;
+                                    } else {
+                                        return 0;
+                                    }
+                                }
+                            });
+                        }
+                        //다운로드 순으로 data 1~5 정렬
                     }
                     Configuration config = new Configuration();
                     config.locale = lang;
                     getResources().updateConfiguration(config, getResources().getDisplayMetrics());
-                    Intent intent = new Intent(getActivity(), MainActivity.class);
-                    startActivity(intent);
-                    getActivity().finish();
+                    /*
+                    *
+                    * 여기 부분 까지 오면 data1 ~ data5가 원하는 정렬 방식으로 모두 정렬되있으니까 ListView만 새로고침해줘
+                    *
+                    * */
                     Toast.makeText(getActivity(), text + getResources().getString(R.string.change_lang), Toast.LENGTH_SHORT).show();
                 }
             }
@@ -105,7 +172,7 @@ public class CategoryDetailFragment extends Fragment {
         ArrayList<String> items = new ArrayList<String>();
         items.add("정렬방식");
         items.add("평점순서");
-        items.add("한국인들이 쓰는 순서");
+        items.add("한국인들이 많이 쓰는 순서");
         items.add("다운로드 많이 받은 순");
         ArrayAdapter<String> spiAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, items);
         spinner.setAdapter(spiAdapter);
@@ -113,7 +180,7 @@ public class CategoryDetailFragment extends Fragment {
         return linearLayout;
     }
 
-    public ArrayList DB_read(String datail_category,final Context context) {
+    public ArrayList DB_read(String datail_category, final Context context) {
         final ArrayList<CategoryDetailItem> return_data = new ArrayList<CategoryDetailItem>();
 
         /*DB*/
@@ -122,7 +189,6 @@ public class CategoryDetailFragment extends Fragment {
         DatabaseReference category_Ref = rootRef.child("app_category");
         DatabaseReference delivery_Ref = category_Ref.child(datail_category);
         DatabaseReference delivery_apps_Ref = delivery_Ref.child("apps");
-
 
 
         //final String finalSub_String = sub_String;
@@ -135,34 +201,32 @@ public class CategoryDetailFragment extends Fragment {
                 for (DataSnapshot ds : dataSnapshot.getChildren()) {
                     int aver_rank = 0;
                     int comments_size = 0;
-                    //System.out.println(ds.getKey().toString().trim());
-                    //System.out.println(ds.child("app_img").getValue().toString().trim());
-                    for(DataSnapshot ds2 : ds.child("comments").getChildren()){
+
+                    for (DataSnapshot ds2 : ds.child("comments").getChildren()) {
                         aver_rank += Integer.parseInt(ds2.child("rank").getValue().toString().trim());
                         comments_size++;
                     }
-                    if(comments_size != 0 ) {
+                    if (comments_size != 0) {
                         aver_rank = aver_rank / comments_size;
-                    }else{
+                    } else {
                         aver_rank = 0;
                     }
 
                     DatabaseReference ref = ds.getRef();
 
-
-                    if(ref.getParent().getParent().toString().equals("https://all-for-b9d5b.firebaseio.com/app_category/transport")) {
-                        sub_String = (String)context.getText(R.string.sub_transport);
-                    } else if(ref.getParent().getParent().toString().equals("https://all-for-b9d5b.firebaseio.com/app_category/tour")) {
-                        sub_String = (String)context.getText(R.string.sub_tour);
-                    }else if(ref.getParent().getParent().toString().equals("https://all-for-b9d5b.firebaseio.com/app_category/realstate")) {
-                        sub_String = (String)context.getText(R.string.sub_realstate);
-                    }else if(ref.getParent().getParent().toString().equals("https://all-for-b9d5b.firebaseio.com/app_category/restaurant")) {
-                        sub_String = (String)context.getText(R.string.sub_restaurant);
-                    }else if(ref.getParent().getParent().toString().equals("https://all-for-b9d5b.firebaseio.com/app_category/delivery")) {
-                        sub_String = (String)context.getText(R.string.sub_delivery);
+                    if (ref.getParent().getParent().toString().equals("https://all-for-b9d5b.firebaseio.com/app_category/transport")) {
+                        sub_String = (String) context.getText(R.string.sub_transport);
+                    } else if (ref.getParent().getParent().toString().equals("https://all-for-b9d5b.firebaseio.com/app_category/tour")) {
+                        sub_String = (String) context.getText(R.string.sub_tour);
+                    } else if (ref.getParent().getParent().toString().equals("https://all-for-b9d5b.firebaseio.com/app_category/realstate")) {
+                        sub_String = (String) context.getText(R.string.sub_realstate);
+                    } else if (ref.getParent().getParent().toString().equals("https://all-for-b9d5b.firebaseio.com/app_category/restaurant")) {
+                        sub_String = (String) context.getText(R.string.sub_restaurant);
+                    } else if (ref.getParent().getParent().toString().equals("https://all-for-b9d5b.firebaseio.com/app_category/delivery")) {
+                        sub_String = (String) context.getText(R.string.sub_delivery);
                     }
 
-                    return_data.add(new CategoryDetailItem(ds.getKey().toString().trim(), sub_String, aver_rank, ds.child("app_img").getValue().toString().trim()));
+                    return_data.add(new CategoryDetailItem(ds.getKey().toString().trim(), sub_String, aver_rank, ds.child("app_img").getValue().toString().trim(),Integer.parseInt(ds.child("download_rank").getValue().toString().trim()),Integer.parseInt(ds.child("korean_use_rank").getValue().toString().trim())));
                 }
 
             }
@@ -178,7 +242,7 @@ public class CategoryDetailFragment extends Fragment {
         return return_data;
     }
 
-    public void setToCall (String toCall){
+    public void setToCall(String toCall) {
 
         this.toCall = toCall;
 
@@ -198,25 +262,25 @@ public class CategoryDetailFragment extends Fragment {
         categoryDetailAdapter.notifyDataSetChanged();
     }
 
-    public void setViewPager (CustomViewPager viewPager){
+    public void setViewPager(CustomViewPager viewPager) {
         this.viewPager = viewPager;
     }
 
-    private void setTransport () {
+    private void setTransport() {
         detail.setText("Transport");
         detail_icon.setImageResource(R.drawable.transport_black);
         categoryDetailAdapter.setData(data1);
         detail_explain.setText(getText(R.string.introduceTrans));
     }
 
-    private void setRestaurant () {
+    private void setRestaurant() {
         detail.setText("Restaurant");
         detail_icon.setImageResource(R.drawable.restaurant_black);
         categoryDetailAdapter.setData(data2);
         detail_explain.setText(getText(R.string.introduceRest));
     }
 
-    private void setDelivery () {
+    private void setDelivery() {
         detail.setText("Food Delivery");
         detail_icon.setImageResource(R.drawable.food_delivery_black);
         categoryDetailAdapter.setData(data3);
@@ -224,26 +288,26 @@ public class CategoryDetailFragment extends Fragment {
 
     }
 
-    private void setProperty () {
+    private void setProperty() {
         detail.setText("Real Property");
         detail_icon.setImageResource(R.drawable.house_black);
         categoryDetailAdapter.setData(data5);
         detail_explain.setText(getText(R.string.introducePro));
     }
 
-    private void setTravel () {
+    private void setTravel() {
         detail.setText("Travel");
         detail_icon.setImageResource(R.drawable.travel_black);
         categoryDetailAdapter.setData(data4);
         detail_explain.setText(getText(R.string.introduceTravel));
     }
 
-    public void dataRefresh(Context context){
-        data1 = DB_read("transport",context);
-        data2 = DB_read("restaurant",context);
-        data3 = DB_read("delivery",context);
-        data4 = DB_read("tour",context);
-        data5 = DB_read("realstate",context);
+    public void dataRefresh(Context context) {
+        data1 = DB_read("transport", context);
+        data2 = DB_read("restaurant", context);
+        data3 = DB_read("delivery", context);
+        data4 = DB_read("tour", context);
+        data5 = DB_read("realstate", context);
     }
 
 }
--- app/src/main/java/com/example/qpdjg/all_for/Item/CategoryDetailItem.java
+++ app/src/main/java/com/example/qpdjg/all_for/Item/CategoryDetailItem.java
@@ -1,18 +1,28 @@
 package com.example.qpdjg.all_for.Item;
 
 
-
 public class CategoryDetailItem {
     String appname;
     String subname;
     int rank;
     String icon;
+    int download_rank;
+    int korean_use_rank;
 
     public CategoryDetailItem(String appname, String subname, int rank, String icon) {
         this.appname = appname;
         this.subname = subname;
         this.rank = rank;
         this.icon = icon;
+    }
+
+    public CategoryDetailItem(String appname, String subname, int rank, String icon, int download_rank, int korean_use_rank) {
+        this.appname = appname;
+        this.subname = subname;
+        this.rank = rank;
+        this.icon = icon;
+        this.download_rank = download_rank;
+        this.korean_use_rank = korean_use_rank;
     }
 
     public String getAppname() {
@@ -30,4 +40,8 @@ public class CategoryDetailItem {
     public String getIcon() {
         return icon;
     }
+
+    public int getKorean_use_rank() { return korean_use_rank; }
+
+    public int getDownload_rank() { return download_rank; }
 }
